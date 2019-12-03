4b5c96a844795eb0988d153bfdf302f7f8189f7c
John Mitsch <jomitsch@redhat.com>
2019-12-03 00:56:56 -0500

    Added google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { ServerStyleSheets } from "@material-ui/core/styles";
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import GA_TRACKING_ID from "../lib/gtag";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -43,6 +44,21 @@ class MyDocument extends Document {
             content="/static/favicons/bro /wserconfig.xml"
           />
           <meta name="theme-color" content="#ffffff" />
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}');
+          `
+            }}
+          />
         </Head>
         <body>
           <NextScript />
